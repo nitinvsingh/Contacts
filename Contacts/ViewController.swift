9163ba3736830f0f3df3a56f0b3f69da030a778a
nitinvsingh <nitinvsingh@gmail.com>
2019-02-14 16:04:09 +0530

    #1 Created project. Nothing here, just the default Single View App template files.

---
+++ Contacts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Contacts
+//
+//  Created by nitin-7926 on 14/02/19.
+//  Copyright © 2019 Nitin Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
